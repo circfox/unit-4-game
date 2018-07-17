da52f44bce6ead36c3d9063b9bca55ea4c603129
Ray Ventura <circfox@yahoo.com>
2018-07-17 04:08:58 -0700

    modified files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,137 @@
+$(document).ready(function () {
+  // First initialize global variables
+  var wins = 0;
+  var losses = 0;
+  var counter = 0;
+  var gameRun = 0;
+  var crystalValue = 0;
 
-// First initialize global variables
-var wins = 0;
-var losses = 0;
-var num1 = 0;
-var ranNumArr = [];
+  var targetNumber = $("#random-number-box");
+  $("#wins").text(wins);
+  $("#losses").text(losses);
 
-var targetNumber = $("#random-number-box");
-$("#wins").text(wins);
-$("#losses").text(losses);
-// Establish a random number  between 19 to 120 
-//$(document).ready(function(){
-targetNumber = (Math.floor(Math.random() * 101) + 19);
-//});
-// update random the "random-number-box" with the targetNumber using jQuery to select id random-target-no. 
-var fillSpan = $("#random-target-no");
-fillSpan.append(targetNumber);
-$("#random-target-no").append(fillSpan);
+  // Establish a random number  between 19 to 120 
+  targetNumber = (Math.floor(Math.random() * 101) + 19);
 
+  // update random the "random-number-box" 
+  $("#random-target-no").text(targetNumber);
+  console.log(targetNumber);
 
-//$("#random-number-box").text(targetNumber);
-console.log(targetNumber);
+  //generate random no for each crystal
+  num1 = (Math.floor(Math.random() * 12) + 1); console.log(num1);
+  num2 = (Math.floor(Math.random() * 12) + 1); console.log(num2);
+  num3 = (Math.floor(Math.random() * 12) + 1); console.log(num3);
+  num4 = (Math.floor(Math.random() * 12) + 1); console.log(num4);
 
-
-
-// set and initialize a variable counter to 0
-var counter = 0;
-//var increment = numberOptions[Math.round(Math.random())];
-//generate 4 numbers that can randomly cover the numbers from 1 to 12
-
-//array to hold random numbers
-
-
-
-
-  //generates random no from 1 to 3
-  num1 = (Math.floor(Math.random() * 3)+1); 
-  ranNumArr.push(num1);
-  //$("#crystal-1")
-
-  //generates random no from 4 to 6
-
-  num2 = (Math.ceil(Math.random() * 3)+ 3); 
-  //$("#crystal-2")
-  ranNumArr.push(num2);
- //generates random no from 7 to 9
-
-  num3 = (Math.floor(Math.random() * 3)+7); 
-  //$("#crystal-2")
-  ranNumArr.push(num3);
- //generates random no from 10 to 12
-
-  num4 = (Math.ceil(Math.random() * 3)+9); 
-  //$("#crystal-2")
-  ranNumArr.push(num4);
-  console.log(ranNumArr);
-  
-  //Create a for loop to assign number to each crystl image
-  for (var i = 0; i < ranNumArr.length; i++) {
-  //
-  var imageOption = $(".crystals");
-  // Add class for each crystal 
-  //imageOption.addId("crystal-1","crystal-2","crystal-3","crystal-4");
-
+  //Reset game
+  function reset() {
+    gameRun = 1;
+    targetNumber = (Math.floor(Math.random() * 101) + 19);
+    $("#random-target-no").text(targetNumber);
+    num1 = (Math.floor(Math.random() * 12) + 1); console.log(num1);
+    num2 = (Math.floor(Math.random() * 12) + 1); console.log(num2);
+    num3 = (Math.floor(Math.random() * 12) + 1); console.log(num3);
+    num4 = (Math.floor(Math.random() * 12) + 1); console.log(num4);
+    crystalValue = 0;
+    $("#total-score").text(crystalValue);
   }
+
+  //checking if game needs to restart
+  function proceed() {
+    if ((gameRun == 0 && crystalValue > targetNumber) || (crystalValue == targetNumber)) {
+      reset();
+      alert("New game coming up!");
+      } else {
+      gameRun = 1;
+    }
+  }
+
+  function conditions() {
+        // conditions
+        if (crystalValue == targetNumber){
+          $("#total-score").text(crystalValue);
+          gameRun = 0;
+          letTheGoodTimesRoll();
+        }  
+          else if (crystalValue > targetNumber){
+            gameRun = 0;
+            badTimes();
+          }
+    
+  }
+
+  // on clicking crystals
+  $("#crystal-1").on("click", function(){
+    proceed();
+    //console.log(num1);
+    crystalValue = crystalValue + num1;
+    //console.log("Your total score is " + crystalValue);
+    $("#total-score").text(crystalValue);
+    conditions();
+    
+})
+  $("#crystal-2").on("click", function(){
+    proceed();
+    crystalValue = crystalValue + num2;
+    //console.log("Your total score is " + crystalValue);
+    $("#total-score").text(crystalValue);
+    // conditions
+      if (crystalValue == targetNumber){
+        gameRun = 0;
+        letTheGoodTimesRoll();
+      }  
+        else if (crystalValue > targetNumber){
+          gameRun = 0;
+          badTimes();
+        }
+  })
+
+  $("#crystal-3").on("click", function(){
+    proceed();
+    crystalValue = crystalValue + num3;
+    //console.log("Your total score is " + crystalValue);
+    $("#total-score").text(crystalValue);
+    // conditions
+      if (crystalValue == targetNumber){
+        gameRun = 0;
+        letTheGoodTimesRoll();
+      }  
+        else if (crystalValue > targetNumber){
+          gameRun = 0;
+          badTimes();
+        }
+  })
+
+  $("#crystal-4").on("click", function(){
+    proceed();
+    crystalValue = crystalValue + num4;
+    console.log("Your total score is " + crystalValue);
+    $("#total-score").text(crystalValue);
+    // conditions
+      if (crystalValue == targetNumber){
+        gameRun = 0;
+        letTheGoodTimesRoll();
+      }  
+        else if (crystalValue > targetNumber){
+          gameRun = 0;
+          badTimes();
+        }
+  })
+ // Display wins
+  function letTheGoodTimesRoll() {
+    alert("EWOK CELEBRATION! YOU WON!");
+    wins++;
+    $("#wins").text(wins);
+    gameRun = 0;
+    proceed();
+  }
+  function badTimes() {
+    alert("You lose! Nada Celebration");
+    losses++
+    $("#losses").text(losses);
+    gameRun = 0;
+    proceed();
+  }
+
+  });
+
